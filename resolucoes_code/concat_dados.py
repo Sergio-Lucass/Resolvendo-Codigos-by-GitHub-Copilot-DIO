492245086abb0a7ddea79cdb56455e69531b611e
Sérgio Lucas Gomes da Silva Rodrigues <sergio.rod.guapi@gmail.com>
2026-01-15 16:31:45 -0300

    Adicionando os arquivos já feitos

---
+++ resolucoes_code/concat_dados.py
@@ -0,0 +1,9 @@
+# Vamos receber dois arquivos de dados diferentes de usuário e concatená-los em uma única string.
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+# Concatenando as duas informações em uma única string
+dados_concatenados = info1 + " " + info2
+
+print("As informações concatenadas são: ", dados_concatenados)
---
+++ resolucoes_code/ope_mat.py
@@ -0,0 +1,28 @@
+# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+operacao = input("Digite a operação (+, -, *, /): ")
+
+if operacao == "+": 
+    resultado = num1 + num2
+    print(f"O resultado de {num1} + {num2} é {resultado}")
+
+elif operacao == "-":
+    resultado = abs(num1 - num2)
+    print(f"O resultado de {num1} - {num2} é {resultado}")
+
+elif operacao == "*":
+    resultado = num1 * num2
+    print(f"O resultado de {num1} * {num2} é {resultado}")
+
+elif operacao == "/":
+    if num2 != 0:
+        resultado = num1 / num2
+        print(f"O resultado de {num1} / {num2} é {resultado}")
+    else:
+        print("Erro: Divisão por zero não é permitida.")
+
+else:
+    print("Operação inválida. Por favor, escolha entre +, -, * ou / e rode o código novamente.")
+
